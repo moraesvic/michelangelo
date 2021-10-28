254cef6c69a7812fa8eee4000e95417f63e80dc5
Victor Moraes <victor@vulpi-ideas.com>
2021-10-28 19:31:59 -0300

    Restructuring project directory tree and removing bash dependencies

--- run_dev.sh
+++ run_dev.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-export FLASK_APP="app.py"
+# Activate virtual environment
+this_dir="$(dirname "$0")"
+source "$this_dir/venv/bin/activate"
+
+export FLASK_APP="$this_dir/app.py"
 export FLASK_ENV="development"
 export PORT=7777
 flask run --port="$PORT"
--- run_prod.sh
+++ run_prod.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-(cd client && npm run build) || exit 1
-FLASK_APP="app.py"
+# Activate virtual environment
+this_dir="$(dirname "$0")"
+source "$this_dir/venv/bin/activate"
+
+(cd "$this_dir/client" && npm run build) || exit 1
+FLASK_APP="$this_dir/app.py"
 export FLASK_ENV="production"
 export PORT=7777
 python3 "$FLASK_APP"
