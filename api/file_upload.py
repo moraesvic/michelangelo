9fb1ecf63e4e754d57d6ef218006ca97df9ee437
Victor Moraes <victor@vulpi-ideas.com>
2021-10-30 13:52:57 -0300

    all tests passedgit add .

--- api/file_upload.py
+++ api/file_upload.py
@@ -1,4 +1,4 @@
-import os, random, time, math
+import os, re, random, time, math, subprocess
 
 # We will need to import this, as native support to "list of integers" kind
 # of type hinting was only added in Python3.9
@@ -71,7 +71,7 @@ def save_file(
     # supposed to serve as an independent piece of software, usable by other
     # frameworks, we will check file size again.
 
-    if max_size == 0 or check_size_before_saving:
+    if check_size_before_saving:
         file_storage.seek(0, os.SEEK_END)
         file_size = file_storage.tell()
     else:
@@ -95,14 +95,77 @@ def get_mime_type(path: str) -> Tuple[str, str]:
     """
     Returns (type, subtype)
     """
-    pass
+
+    # Python has libraries for checking mimetype, but this solution was already
+    # tested and proven in another project I did using Node.js
+    # For future reference:
+    # https://stackoverflow.com/questions/43580/how-to-find-the-mime-type-of-a-file-in-python
+
+    arg = f"file --mime-type \"{path}\""
+    output = subprocess.check_output(arg, shell = True, text = True)
+    groups = re.search(r"^.+?: (\w+)/(\w+)", output).groups()
+    return groups[0], groups[1]
 
 def test_mime_type(path: str, accepted_types: List[str]) -> bool:
-    pass
+    """
+    Tests whether the given file's mimetype is in the accepted_types
+    """
+    try:
+        file_type, _ = get_mime_type(path)
+        return file_type in accepted_types
+    except:
+        return False
 
-def rename_mime_type(path: str) -> str:
-    pass
+def rename_mime_type(path: str, subtype: str = None) -> str:
+    """
+    Renames the file according to its mime subtype
+    """
+    # Please note that text files will not be renamed as file.txt, but as
+    # file.plain . This makes this function limited, in this case, but
+    # for image files it works perfectly
+
+    if subtype:
+        file_subtype = subtype
+    else:
+        _, file_subtype = get_mime_type(path)
+
+    # We do not want something like file.png.png, so we have to check if
+    # file already has the extension
+    regex = re.compile(rf"^.+?\.({file_subtype})$", re.IGNORECASE)
+    basename = os.path.basename(path)
+
+    if not regex.search(basename):
+        new_path = f"{path}.{file_subtype}"
+        os.rename(path, new_path)
+    else:
+        new_path = path
+
+    return new_path
     
+def test_and_rename_mime_type(path: str, accepted_types: List[str]) -> str:
+    """
+    If file's mimetype is among the accepted_types, then we renamed it.
+    If it is not, raises an exception
+    """
+    file_type, file_subtype = get_mime_type(path)
+    if file_type not in accepted_types:
+        raise Exception("File is not in the accepted types!")
+
+    new_path = rename_mime_type(path, subtype = file_subtype)
+    return new_path
+
 def get_md5_hash(path: str) -> str:
-    pass
+    arg = f"md5sum \"{path}\""
     
+    try:
+        output = subprocess.check_output(arg, shell = True, text = True)
+    except subprocess.CalledProcessError:
+        raise Exception("Failed to calculate MD5 hash for file!")
+
+    regex = re.compile(r"^([0-9a-f]{32}).*$")
+    match = regex.search(output)
+
+    if not match:
+        raise Exception("Failed to calculate MD5 hash for file!")
+
+    return match.groups()[0]
--- api/file_upload_test.py
+++ api/file_upload_test.py
@@ -1,10 +1,9 @@
-import sys, time, re, random, math, unittest
+import sys, time, re, random, math, unittest, subprocess
 from typing import List, Tuple, Dict, Callable
 from werkzeug.datastructures import FileStorage
 
 from file_upload import *
 
-THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 NUMBER_OF_TESTS = 50
 VERBOSE = any([re.search(r"-v", arg) for arg in sys.argv])
 
@@ -13,40 +12,68 @@ def printv(s: str):
     if VERBOSE:
         print(s)
 
+def rel_path(path):
+    this_dir = os.path.dirname(os.path.realpath(__file__))
+    return os.path.join(this_dir, path)
+
 class FileUploadTest(unittest.TestCase):
+
+    # When we are done with tests, we need to close files and remove
+    # all files saved to disk
 
     open_files = []
     tmp_files = []
 
+    # Let's say we rename a file A to the name B. We need to keep track
+    # of this and then revert these changes in the end
+
+    revert_name = []
+
     def open_file(self, path):
         # A wrapper to open files safely into a Werkzeug object
-        filename = os.path.join(THIS_DIR, path)
+        filename = rel_path(path)
         fd = open(filename, 'rb')
         self.open_files.append(fd)
         return FileStorage(fd)
 
     def get_valid_file(self):
-        # This refers to a Python script of normal size ( < 10 KB)
-        return self.open_file("file_upload.py")
+        # This refers to a normal tiny text file
+        return self.open_file("test_files/normal_file.txt")
 
     def get_large_file(self):
         # This refers to a 10MB file with random binary data, generated with
         # "dd if=/dev/urandom of=large_file.bin bs=1024 count=$((10 * 1024))"
 
         # As the default FILE_MAX_SIZE is 5MB, this should give an error
-        return self.open_file("large_file.bin")
+        return self.open_file("test_files/large_file.bin")
 
     @classmethod
     def tearDownClass(cls):
-        # When we are done with tests, we need to close files and remove
-        # all files saved to disk
         printv("Closing files")
         for file in cls.open_files:
             file.close()
 
+        if VERBOSE:
+            print("Printing contents of test directory before cleaning up")
+            print("***")
+
+            test_files_path = rel_path("test_files")
+            print(subprocess.check_output(
+                f"ls {test_files_path}",
+                shell = True,
+                text = True
+                ))
+
+            print("***")
+
         printv("Removing temporary files")
         for file in cls.tmp_files:
             os.remove(file)
+
+        printv("Reverting file names")
+        for name_pair in cls.revert_name:
+            printv(f"renaming {name_pair[0]} to {name_pair[1]}")
+            os.rename(name_pair[0], name_pair[1])
 
     def get_filename_regex(self):
         milliseconds_length = len(str(int(time.time() * 1000)))
@@ -75,7 +102,7 @@ class FileUploadTest(unittest.TestCase):
         with self.assertRaisesRegex(Exception, "File is too large!"):
             save_file(
                 file,
-                ".",
+                rel_path("test_files"),
                 max_size = FILE_MAX_SIZE,
                 check_size_before_saving = False)
 
@@ -86,7 +113,7 @@ class FileUploadTest(unittest.TestCase):
         with self.assertRaisesRegex(Exception, "File is too large!"):
             save_file(
                 file,
-                ".",
+                rel_path("test_files"),
                 max_size = FILE_MAX_SIZE,
                 check_size_before_saving = True)
 
@@ -96,30 +123,101 @@ class FileUploadTest(unittest.TestCase):
         file = self.get_large_file()
         file_path = save_file(
                 file,
-                ".",
+                rel_path("test_files"),
                 max_size = 0)
 
         printv(f"File saved to {file_path}")
         file_basename = os.path.basename(file_path)
         
-        if self.assertRegex(file_basename, self.get_filename_regex()):
-            self.tmp_files.append(file_path)
+        self.assertRegex(file_basename, self.get_filename_regex())
+        self.tmp_files.append(file_path)
 
     def test_save_file_ok(self):
         # This should run successfully
         file = self.get_valid_file()
         file_path = save_file(
                 file,
-                ".",
+                rel_path("test_files"),
                 max_size = 0)
 
         printv(f"File saved to {file_path}")
         file_basename = os.path.basename(file_path)
         
-        if self.assertRegex(file_basename, self.get_filename_regex()):
-            self.tmp_files.append(file_path)
+        self.assertRegex(file_basename, self.get_filename_regex())
+        self.tmp_files.append(file_path)
 
+    def test_get_mime_type_img(self):
+        return_value = get_mime_type(rel_path("test_files/logo.png"))
+        expected = ("image", "png")
+        self.assertTupleEqual(return_value, expected)
 
+    def test_get_mime_type_txt(self):
+        return_value = get_mime_type(rel_path("test_files/normal_file.txt"))
+        expected = ("text", "plain")
+        self.assertTupleEqual(return_value, expected)
+
+    def test_get_mime_type_img(self):
+        # Here we try to fool the method, by passing an image file
+        # with text extension. Doesn't really matter, since Unix does not
+        # care about extensions, and "file" util does not take it into
+        # consideration
+
+        return_value = get_mime_type(rel_path("test_files/logo.txt"))
+        expected = ("image", "png")
+        self.assertTupleEqual(return_value, expected)
+
+    def test_test_mime_type_true(self):
+        self.assertTrue(test_mime_type(
+            rel_path("test_files/logo.txt"),
+            ["image"]
+        ))
+
+    def test_test_mime_type_false(self):
+        self.assertFalse(test_mime_type(
+            rel_path("test_files/normal_file.txt"),
+            ["image"]
+        ))
+
+    def test_rename_mime_type(self):
+        old_path = rel_path("test_files/file_without_extension")
+        new_path = rename_mime_type(old_path)
+        basename = os.path.basename(new_path)
+        self.assertEqual(basename, "file_without_extension.png")
+
+        # Necessary to keep track of renaming to rollback later
+        self.revert_name.append((new_path, old_path))
+
+    def test_test_and_rename_mime_type(self):
+        accepted_types = ["image"]
+
+        # This is a PNG file without the extension
+        old_path_1 = rel_path("test_files/other_file_wo_ext")
+        new_path_1 = test_and_rename_mime_type(old_path_1, accepted_types)
+        self.assertRegex(os.path.basename(new_path_1), "other_file_wo_ext.png")
+        self.revert_name.append((new_path_1, old_path_1))
+
+        # This is a text file (should raise exception)
+        old_path_2 = rel_path("test_files/normal_file.txt")
+        with self.assertRaisesRegex(Exception, "File is not in the accepted types!"):
+            test_and_rename_mime_type(old_path_2, accepted_types)
+
+        # This is a PNG file that already has the extension .png
+        old_path_3 = rel_path("test_files/logo.png")
+        new_path_3 = test_and_rename_mime_type(old_path_3, accepted_types)
+        self.assertRegex(os.path.basename(new_path_3), "logo.png")
+        
+        # No need to revert name, as there was no renaming
+
+    def test_get_md5_hash(self):
+        path_1 = rel_path("test_files/logo.png")
+        hash_1 = get_md5_hash(path_1)
+        expected = "367794bc62a0b8ea08d6e982d3db0827"
+        self.assertRegex(hash_1, expected)
+
+        path_2 = rel_path("nonexistent/path")
+        with self.assertRaisesRegex(Exception, "Failed to calculate MD5 hash for file!"):
+            get_md5_hash(path_2)
+        
 
 if __name__ == "__main__":
     unittest.main()
